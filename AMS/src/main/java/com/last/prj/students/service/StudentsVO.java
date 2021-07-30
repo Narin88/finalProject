5754265a4735de79693e70e669b70a0ce496355b
admin <admin>
2021-07-30 17:54:43 +0900

    0730 오전 오후 ㅡㅡㅡㅡ 오후 포함!!

--- AMS/src/main/java/com/last/prj/students/service/StudentsVO.java
+++ AMS/src/main/java/com/last/prj/students/service/StudentsVO.java
@@ -26,4 +26,41 @@ public class StudentsVO {
 	private int sgender;		// 학생 성별
 	private int term;			// 학기
 	private String disabled;	// 장애 여부
+	
+	
+	
+	// 아래는 students 테이블 외의 값을 받아오기 위한 변수
+
+	// score_mana 테이블 ( 성적 관리 & 수강 등록 )
+	private int total;			// 총점
+	private String rank;		// 등급
+	
+	// lecture 테이블 ( 강의 개설 )
+//		private int term;		// 학기
+	private String lnum;		// 강의 번호
+	private String lname;		// 강의명
+	private String lyear;		// 강의 생성 년도
+	private String dividenum;	// 분반
+	private String timetable;	// 강의 시간(대략적) ex) 월1, 화2
+	
+	// lecture_info 테이블 (강의 정보)
+	private String division;	// 이수 구분
+	private String dcode;		// 이수 구분 코드
+	private String target;		// 대상 학년
+	
+	// lecture_room 테이블 ( 강의실 테이블 )
+	private String lrname;		// 강의실명
+	private String location;	// 강의실 있는 지역
+	
+	// division_info 테이블 (이수 구분)	
+	private int credit;			// 학점
+	
+	// major 테이블 ( 학과 )
+	private String mname;		// 학과명
+	
+	// professor 테이블 ( 교수 )
+	private String pname;		// 교수명
+	
+	// common_detail_code ( 공통 상세 코드 )
+	private String student_status;	// 학생 학적 구분
 }
--- AMS/src/main/java/com/last/prj/students/service/impl/StudentsMapper.java
+++ AMS/src/main/java/com/last/prj/students/service/impl/StudentsMapper.java
@@ -1,18 +1,18 @@
-package com.last.prj.students.service;
+package com.last.prj.students.service.impl;
 
 import java.util.List;
 
 import com.last.prj.professor.service.ProfessorVO;
+import com.last.prj.students.service.StudentsVO;
 
-public interface StudentsService {
-	
+public interface StudentsMapper {
+
 	StudentsVO studentInfo(StudentsVO vo);			// 학적 조회
 	List<StudentsVO> scoreView(StudentsVO vo);		// 성적 조회
 	List<StudentsVO> appliedLecture(StudentsVO vo);	// 신청한 강의 조회
 	List<StudentsVO> lectureLookUp(StudentsVO vo);	// 강의 시간표들 조회
-
+	
 	ProfessorVO professorSelect(ProfessorVO vo);	// 교수 정보 조회
 	
 	int studentUpdate(StudentsVO vo);				// 학적 수정
-	
 }

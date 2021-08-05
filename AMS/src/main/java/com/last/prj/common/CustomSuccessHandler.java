1ddc7f7ccd5c74c90fd8666908cb30e3d1c3a230
Narin88 <dlrjatk1@gmail.com>
2021-08-05 09:27:08 +0900

    0927

--- AMS/src/main/java/com/last/prj/common/CustomSuccessHandler.java
+++ AMS/src/main/java/com/last/prj/common/CustomSuccessHandler.java
@@ -1,4 +1,4 @@
-package com.last.prj.log.service;
+package com.last.prj.common;
 
 import java.io.IOException;
 import java.util.Map;
@@ -14,6 +14,7 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
 
+import com.last.prj.log.service.LogService;
 import com.last.prj.log.web.LogInfo;
 
 public class CustomSuccessHandler extends SimpleUrlAuthenticationSuccessHandler {

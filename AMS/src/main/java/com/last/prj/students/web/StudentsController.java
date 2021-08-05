f7b682a7926dbe00323e8a035407f87d2bcc25a3
Narin88 <dlrjatk1@gmail.com>
2021-08-05 22:36:50 +0900

    123

--- AMS/src/main/java/com/last/prj/students/web/StudentsController.java
+++ AMS/src/main/java/com/last/prj/students/web/StudentsController.java
@@ -8,6 +8,7 @@ import javax.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.last.prj.students.service.StudentsService;
@@ -152,4 +153,7 @@ public class StudentsController {
 		return path;
 	}
 
+	
+	// ajax 시험성적 검색기능
+//	@PostMapping("")
 }

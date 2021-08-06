2cb8e8254b4a0cd9c3b227337dfd73f5f443b319
Dio <Dio@LAPTOP-8D4IRP1U>
2021-08-06 17:41:26 +0900

    ㅇㅇ

--- AMS/src/main/java/com/last/prj/evaluation/service/evaluationVO.java
+++ AMS/src/main/java/com/last/prj/evaluation/service/evaluationVO.java
@@ -1,5 +1,16 @@
 package com.last.prj.evaluation.service;
 
-public class evaluationVO {
+import java.sql.Date;
 
+public class evaluationVO {
+	private int sid; //학번
+	
+	
+	private String title; // 강의 평가 제목
+	
+	private Date sdate; // 시작 일 
+	private Date edate; // 마감 일 
+	private String section; // 질문 구분
+	private String course;
+	private int rnumber;
 }

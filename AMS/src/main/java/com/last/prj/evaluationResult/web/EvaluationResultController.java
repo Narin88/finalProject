52e9acff3eb587675404bd96abcf4fa2b18af398
Dio <Dio@LAPTOP-8D4IRP1U>
2021-08-12 14:08:57 +0900

    chart

--- AMS/src/main/java/com/last/prj/evaluationResult/web/EvaluationResultController.java
+++ AMS/src/main/java/com/last/prj/evaluationResult/web/EvaluationResultController.java
@@ -58,8 +58,10 @@ public class EvaluationResultController {
 	@ResponseBody
 	public Map<String, Object> getEvaluationResultpr(EvaluationResultVO vo){
 			Map<String, Object> map = new HashMap<String,Object>();
+			vo.setOpennum("14");
 			vo.setA1("a1");
-			map.put("q1", dao.EvaluationResult(vo));
+			System.out.println(dao.EvaluationResult(vo));
+			map.put("q1", dao.EvaluationResult(vo));  
 			vo.setA1("a2");
 			map.put("q2", dao.EvaluationResult(vo));
 			vo.setA1("a3");
@@ -72,6 +74,8 @@ public class EvaluationResultController {
 			map.put("q6", dao.EvaluationResult(vo));
 			vo.setA1("a7");
 			map.put("q7", dao.EvaluationResult(vo));
+			
+			
 		return map;
 	}
 }
